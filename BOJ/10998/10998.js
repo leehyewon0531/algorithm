238bad7903ea60fe6028acf051d27d1edb9f87cc
leehyewon0531 <ifpffk@naver.com>
2023-01-16 23:54:40 +0900

    feat: add BOJ 10998

---
+++ BOJ/10998/10998.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const input = fs.readFileSync('BOJ/test.txt').toString().trim().split('\n');
+const result = input[0].split(' ').map(el => parseInt(el));
+console.log(result[0] * result[1]);
