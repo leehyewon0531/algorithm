7b4261065fac12622376736cb381865c98a106aa
leehyewon0531 <ifpffk@naver.com>
2023-01-16 23:45:56 +0900

    feat: add BOJ 10950

---
+++ BOJ/10950/10950.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('BOJ/test.txt').toString().trim().split('\n');
+const num = parseInt(input[0]); // test case 개수
+
+function solution() {
+  for(let i = 1; i <= num; i++) {
+    let result = input[i].split(' ').map(el => parseInt(el));
+    console.log(result[0] + result[1]);
+  }  
+}
+
+solution();
