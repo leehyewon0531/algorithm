fb1f23cb359b213af129d7f7199dc4dc86df4d67
leehyewon0531 <ifpffk@naver.com>
2023-01-17 17:11:13 +0900

    feat: add BOJ 11021

---
+++ BOJ/11021/11021.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('BOJ/test.txt').toString().trim().split('\n');
+
+function solution() {
+  for(let i = 1; i < input.length; i++) {
+    let temp = input[i].split(' ').map(el => parseInt(el));
+    const result = temp[0] + temp[1];
+    console.log("Case #"+i+": "+result);
+  }
+}
+
+solution();
