6aab9dc3b25c76d51e40481c61274e668ec33b70
leehyewon0531 <ifpffk@naver.com>
2023-01-17 17:35:30 +0900

    feat: add BOJ 15792 (18)

---
+++ BOJ/15792/15792.js
@@ -0,0 +1,6 @@
+// 18점...
+
+const fs = require('fs');
+const input = fs.readFileSync('BOJ/test.txt').toString().trim().split('\n');
+const result = input[0].split(' ').map(el => parseInt(el));
+console.log((result[0] / result[1]).toFixed(100));
