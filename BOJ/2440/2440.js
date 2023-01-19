84ec1356a4b2cec07e557c43ba5fde050ba0cce3
leehyewon0531 <ifpffk@naver.com>
2023-01-19 23:30:59 +0900

    feat: add BOJ 2440

---
+++ BOJ/2440/2440.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('BOJ/test.txt').toString().trim().split('\n');
+const N = parseInt(input[0]);
+
+function solution() {
+  for(let i = N; i > 0; i--) {
+    let str = "";
+    for(let j = 0; j < i; j++) {
+      str += "*";
+    }
+    console.log(str);
+  }
+}
+
+solution();
