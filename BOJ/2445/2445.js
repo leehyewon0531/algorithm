89d98f29708a6e358651405fe409b087d0e8f18b
leehyewon0531 <ifpffk@naver.com>
2023-02-03 20:42:45 +0900

    feat: add BOJ 2445

---
+++ BOJ/2445/2445.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const input = fs.readFileSync('BOJ/test.txt').toString().trim().split('\n');
+const N = parseInt(input[0]);
+
+let star = 0;
+let blank = 0;
+let line = '';
+
+for(let i = 0; i < N; i++) {
+  star = i + 1;
+  blank = (N - (i + 1)) * 2;
+  line = '';
+
+  for(let j = 0; j < star; j++) {
+    line += '*';
+  }
+
+  for(let j = 0; j < blank; j++) {
+    line += ' ';
+  }
+
+  for(let j = 0; j < star; j++) {
+    line += '*';
+  }
+
+  console.log(line);
+}
+
+for(let i = 1; i < N; i++) {
+  star = N - i;
+  blank = 2 * i;
+  line = '';
+
+  for(let j = 0; j < star; j++) {
+    line += '*';
+  }
+
+  for(let j = 0; j < blank; j++) {
+    line += ' ';
+  }
+
+  for(let j = 0; j < star; j++) {
+    line += '*';
+  }
+
+  console.log(line);
+}
